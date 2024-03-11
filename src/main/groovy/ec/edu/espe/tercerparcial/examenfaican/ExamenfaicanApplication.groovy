3adf9f148ba9b1ab1354e29b12feeaf5a0b19c37
dlfaican <dlfaican@espe.edu.ec>
2024-03-11 18:59:34 -0500

    Creacion

---
+++ src/main/groovy/ec/edu/espe/tercerparcial/examenfaican/ExamenfaicanApplication.groovy
@@ -0,0 +1,13 @@
+package ec.edu.espe.tercerparcial.examenfaican
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ExamenfaicanApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ExamenfaicanApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ec/edu/espe/tercerparcial/examenfaican/ExamenfaicanApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ec.edu.espe.tercerparcial.examenfaican
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExamenfaicanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
