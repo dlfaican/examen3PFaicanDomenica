934e24fb0e1da172679f92a09d06b64951c165f2
dlfaican <dlfaican@espe.edu.ec>
2024-03-11 19:11:21 -0500

    proyecto gradle

---
+++ src/main/groovy/ec/edu/espe/examen3p/faicandomenica/FaicandomenicaApplication.groovy
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen3p.faicandomenica
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FaicandomenicaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FaicandomenicaApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ec/edu/espe/examen3p/faicandomenica/FaicandomenicaApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen3p.faicandomenica
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FaicandomenicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
